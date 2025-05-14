13382be38b8c50a0d54e17f021292b9bbe0fdbec
Dmitriy Borovkov <borovvkovv@gmail.comi>
2025-05-14 10:46:15 +0500

    Правка для GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/TestTask1/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
